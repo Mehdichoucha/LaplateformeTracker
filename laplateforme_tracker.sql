02089905f74f9682031f8cbd4279088e581f8a4f
thibault-manse <thibault.manse@laplateforme.io>
2025-07-11 09:54:13 +0200

    update : update database

--- laplateforme_tracker.sql
+++ laplateforme_tracker.sql
@@ -17,6 +17,138 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: admin; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.admin (
+    id integer NOT NULL,
+    nom_utilisateur character varying(255),
+    motdepasse text NOT NULL
+);
+
+
+ALTER TABLE public.admin OWNER TO postgres;
+
+--
+-- Name: admin_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.admin_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.admin_id_seq OWNER TO postgres;
+
+--
+-- Name: admin_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.admin_id_seq OWNED BY public.admin.id;
+
+
+--
+-- Name: etudiant; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.etudiant (
+    id integer NOT NULL,
+    nom character varying(255) NOT NULL,
+    prenom character varying(255) NOT NULL,
+    age integer NOT NULL
+);
+
+
+ALTER TABLE public.etudiant OWNER TO postgres;
+
+--
+-- Name: etudiant_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.etudiant_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.etudiant_id_seq OWNER TO postgres;
+
+--
+-- Name: etudiant_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.etudiant_id_seq OWNED BY public.etudiant.id;
+
+
+--
+-- Name: note; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.note (
+    id integer NOT NULL,
+    etudiant_id integer NOT NULL,
+    intitule character varying(255) NOT NULL,
+    note integer NOT NULL
+);
+
+
+ALTER TABLE public.note OWNER TO postgres;
+
+--
+-- Name: note_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.note_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.note_id_seq OWNER TO postgres;
+
+--
+-- Name: note_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.note_id_seq OWNED BY public.note.id;
+
+
+--
+-- Name: admin id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.admin ALTER COLUMN id SET DEFAULT nextval('public.admin_id_seq'::regclass);
+
+
+--
+-- Name: etudiant id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.etudiant ALTER COLUMN id SET DEFAULT nextval('public.etudiant_id_seq'::regclass);
+
+
+--
+-- Name: note id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.note ALTER COLUMN id SET DEFAULT nextval('public.note_id_seq'::regclass);
+
+
 --
 -- Data for Name: admin; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -32,7 +164,10 @@ COPY public.admin (id, nom_utilisateur, motdepasse) FROM stdin;
 
 COPY public.etudiant (id, nom, prenom, age) FROM stdin;
 1	Bob	Lennon	34
-2	Emile	Emilio	22
+2	Emile	Emilio	23
+5	Thibault	Manse	22
+6	Joseph	Dmytriyev	26
+7	Mehdi	Choucha	4
 \.
 
 
@@ -43,6 +178,11 @@ COPY public.etudiant (id, nom, prenom, age) FROM stdin;
 COPY public.note (id, etudiant_id, intitule, note) FROM stdin;
 1	1	Soutenance Python	14
 2	2	Soutenance Python	16
+3	5	soutenance	20
+4	6	soutenance	20
+5	6	Tro For	22
+6	5	Tro For	22
+7	7	jeni	25
 \.
 
 
@@ -57,14 +197,46 @@ SELECT pg_catalog.setval('public.admin_id_seq', 1, true);
 -- Name: etudiant_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.etudiant_id_seq', 2, true);
+SELECT pg_catalog.setval('public.etudiant_id_seq', 7, true);
 
 
 --
 -- Name: note_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.note_id_seq', 2, true);
+SELECT pg_catalog.setval('public.note_id_seq', 7, true);
+
+
+--
+-- Name: admin admin_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.admin
+    ADD CONSTRAINT admin_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: etudiant etudiant_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.etudiant
+    ADD CONSTRAINT etudiant_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: note note_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.note
+    ADD CONSTRAINT note_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: note note_etudiant_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.note
+    ADD CONSTRAINT note_etudiant_id_fkey FOREIGN KEY (etudiant_id) REFERENCES public.etudiant(id) ON DELETE CASCADE;
 
 
 --
